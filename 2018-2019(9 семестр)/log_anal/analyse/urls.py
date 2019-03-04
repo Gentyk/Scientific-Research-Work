e7032a4523bc9aa974c9db1ab792883d82d4edee
Gentyk <33gva@mail.ru>
2019-03-04 22:39:44 +0300

    необходимые окончания

--- 2018-2019(9 семестр)/log_anal/analyse/urls.py
+++ 2018-2019(9 семестр)/log_anal/analyse/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from analyse.views import UserView, UsersView, BaseView, AnalyseView
 
 urlpatterns = [
-    url('user/', UserView.as_view()),
-    url('users/', UsersView.as_view()),
-    url('base/', BaseView.as_view()),
-    url('analyse/', AnalyseView.as_view()),
+    url('user/$', UserView.as_view()),
+    url('users/$', UsersView.as_view()),
+    url('base/$', BaseView.as_view()),
+    url('analyse/$', AnalyseView.as_view()),
 ]
